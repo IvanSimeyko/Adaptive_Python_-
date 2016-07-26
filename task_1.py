cdf97763c502710e58f5ea6e1d06786c8313a9a6
Ivan Simeyko <iv.simeyko@gmail.com>
2016-07-26 22:34:45 +0300

    solved task about snail

--- task_1.py
+++ task_1.py
@@ -14,9 +14,22 @@ def squirrels():
     return ans
 
 
-if __name__ == '__maim__':
+def snail():
+    """
+    Snail creeps up the vertical pole of height H feets. Per day it goes A feets up,
+    and per night it goes B feets down. In which day the snail will reach the top of the pole?
+    """
+    h = int(input('Enter the height: '))
+    a = int(input('Enter the day feets: '))
+    b = int(input('Enter the night feets: '))
+    i = a
+    n = 1
+    while i < h:
+        i -= b
+        n += 1
+        i += a
+    return n
 
-    print(squirrels())
-
-
-
+if __name__ == '__main__':
+    print(snail())
+    #print(squirrels())
