438758ea08034139d31b0471712767c734d79ee3
Ivan Simeyko <iv.simeyko@gmail.com>
2016-07-26 22:06:07 +0300

    solved task about squirrels

--- task_1.py
+++ task_1.py
@@ -1,4 +1,22 @@
 
 def squirrels():
-    pass
+    """
+    N squirrels found K nuts and decided to divide them equally.
+    Determine how many nuts each squirrel will get.
+    """
+    i = 0
+    a = []
+    while i < 2:
+        inp = int(input("Enter number: "))
+        a.append(inp)
+        i += 1
+    ans = a[1]//a[0]
+    return ans
+
+
+if __name__ == '__maim__':
+
+    print(squirrels())
+
+
 
